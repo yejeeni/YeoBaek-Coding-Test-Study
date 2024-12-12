05c4f3e018670707d8a273cf082556dd6a148841
saevit <woosaevit@gmail.com>
2024-12-12 21:28:32 +0900

    [PGS]의상/Lv2

--- 우새빛/8주차/[PGS]의상_Lv2.py
+++ 우새빛/8주차/[PGS]의상_Lv2.py
@@ -6,7 +6,7 @@ def solution(clothes):
     # 옷 종류 별로 몇 개씩 있는지 카운트하기
     for name, type in clothes:
         if type not in clothes_type:
-            clothes_type[type] = 0;
+            clothes_type[type] = 0
         clothes_type[type] += 1
     
     # 해당 타입의 옷을 입지 않는 것을 포함하여 코디 경우의 수 구하기
