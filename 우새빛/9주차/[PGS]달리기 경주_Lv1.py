b96123031abdc1103746403bc65f8dcbb3c2171f
saevit <woosaevit@gmail.com>
2024-12-31 17:33:47 +0900

    [PGS]달리기 경주/Lv1

---
+++ 우새빛/9주차/[PGS]달리기 경주_Lv1.py
@@ -0,0 +1,30 @@
+def solution(players, callings): #테스트7 ~ 13 시간초과
+    
+    for c in callings:
+        for i, p in enumerate(players):
+            if p == c:
+                players[i] = players[i-1]
+                players[i-1] = p
+    
+    return players
+
+#시간초과가 가는 이유 예상 : 추월한 선수를 리스트에서 찾는데 오래걸리기 때문에
+#-> 해시를 이용하자자
+
+def solution2(players, callings): #통과
+    
+    ranking = {} #현재 랭킹 정보를 담은 해시 생성
+    for i, p in enumerate(players):
+        ranking[p] = i
+    
+    for c in callings:
+        overtake = ranking[c] #현재 추월한 선수의 순위 찾기
+        
+        temp = players[overtake - 1] #랭킹 업데이트
+        ranking[temp] += 1
+        ranking[c] -= 1
+        
+        players[overtake] = players[overtake - 1] #순위 교체
+        players[overtake - 1] = c
+        
+    return players
