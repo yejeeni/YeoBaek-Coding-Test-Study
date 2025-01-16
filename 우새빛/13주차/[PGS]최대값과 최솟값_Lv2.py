3b514b4dcbcbf3dde2330135eff46d3e8fe47379
saevit <woosaevit@gmail.com>
2025-01-16 21:55:39 +0900

    [PGS]최대값과 최솟값_Lv2

---
+++ 우새빛/13주차/[PGS]최대값과 최솟값_Lv2.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = ''
+    
+    s_list = list(map(int, s.split(" ")))
+    
+    res = []
+    res.append(str(min(s_list)))
+    res.append(str(max(s_list)))
+    
+    answer = " ".join(res)
+    return answer
