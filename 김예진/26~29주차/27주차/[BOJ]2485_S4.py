93024a1c32e6f96ff0a437267413c8dd6020cecb
yejeeni <320418@naver.com>
2025-04-25 09:12:13 +0900

    [BOJ]2485/S4

---
+++ 김예진/26~29주차/27주차/[BOJ]2485_S4.py
@@ -0,0 +1,23 @@
+# 심어져 있는 가로수의 위치가 주어질 때, 모든 가로수가 같은 간격이 되도록 새로 심어야 하는 가로수의 최소수
+# 예를 들어, 가로수가 (1, 3, 7, 13)의 위치에 있다면 (5, 9, 11)의 위치에 가로수를 더 심으면 모든 가로수들의 간격이 같게 된다. 
+# 또한, 가로수가 (2, 6, 12, 18)에 있다면 (4, 8, 10, 14, 16)에 가로수를 더 심어야 한다.
+
+import math
+
+arr = []
+n = int(input())
+for i in range(n):
+    arr.append(int(input()))
+
+distance = []
+for i in range(n-1):
+    # 두 나무의 거리를 배열에 저장
+    distance.append(arr[i+1] - arr[i])
+
+point = math.gcd(*distance)
+
+result = 0
+for d in distance:
+    result += d // point - 1 # 두 나무의 거리를 심어야하는 거리 단위로 나눔
+
+print(result)
