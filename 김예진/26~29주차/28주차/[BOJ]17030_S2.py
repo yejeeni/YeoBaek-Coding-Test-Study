7bfa9a9734ee353f67c702eda0bef5dbab64133e
yejeeni <320418@naver.com>
2025-04-30 09:34:39 +0900

    [BOJ]17030/S2

--- 김예진/26~29주차/28주차/[BOJ]17030_S2.py
+++ 김예진/26~29주차/28주차/[BOJ]17030_S2.py
@@ -1,23 +1,29 @@
 # 2보다 큰 짝수 N이 주어졌을 때, N은 두 소수의 합으로 나타낼 수 있다.
-# 골드바흐 파티션의 수 구하기 (중복 제거)
+# 골드바흐 파티션의 수 구하기 (중복은 카운트 X)
 
-t = int(input())
+import sys
+
+# 1,000,000까지의 소수를 먼저 구해두기
+MAX = 1000000
+arr = [False, False] + [True] * (MAX-1)
+for i in range(2, int(MAX**0.5)+1):
+    if arr[i]:
+        for j in range(i * i, MAX + 1, i):
+            arr[j] = False
+
+t = int(sys.stdin.readline().strip())
 
 for _ in range(t):
-    n = int(input())
-
-    arr = [False, False] + [True] * (n-1)
-
-    for i in range(2, int(n**0.5)+1):
-        if arr[i]:
-            for j in range(i * i, n + 1, i):
-                arr[j] = False
+    n = int(sys.stdin.readline().strip())
 
     cnt = 0
     for i in range(2, n//2 + 1): # 2부터 반절까지만 보면 됨 (중복 제거 겸)
         if arr[i] and arr[n-i]:
             cnt += 1
-        """
+    print(cnt)
+    
+
+    """
         n=6, arr = [F, F, T, T, F, T, F]일 때
              index  0  1  2  3  4  5  6
         
@@ -25,6 +31,4 @@ for _ in range(t):
     ex, 3                          6-3=3    
         골드바흐 파티션이 되는 것
         인덱스가 곧 숟자라서 가능!
-        """
-        
-    print(cnt)
+    """
